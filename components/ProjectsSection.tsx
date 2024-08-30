42c455a4053fb42e1f1ba03164e393afcf43cd47
Khushaal Choithramani <khushaal.choithramani@gmail.com>
2024-08-30 17:58:32 +0530

    new repo

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -63,63 +63,67 @@ function ProjectsSection({
       )
     : openSourceProjects;
 
-  return (
-    <div className="flex flex-col justify-start items-start mt-20 min-h-[80vh]">
-      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-5 mt-10">
-        {filteredProjects.map((data: any, i: number) => (
-          <div key={i} className="border rounded p-5 relative  max-w-lg">
-            <p className="font-bold text-lg bg-gradient-to-t from-purple-800 h-fit via-violet-900 to-purple-800 text-transparent bg-clip-text">
-              {data?.projectName}
-            </p>
+  if (filteredProjects) {
+    return (
+      <div className="flex flex-col justify-start items-start mt-20 min-h-[80vh]">
+        <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-5 mt-10">
+          {filteredProjects.map((data: any, i: number) => (
+            <div key={i} className="border rounded p-5 relative  max-w-lg ">
+              <p className="font-bold text-lg bg-gradient-to-t mt-5 from-purple-800 h-fit via-violet-900 to-purple-800 text-transparent bg-clip-text">
+                {data?.projectName}
+              </p>
 
-            {user?.emailAddresses[0].emailAddress ==
-              data?.projectOwner?.emailAddresses[0]?.emailAddress && (
-              <div
-                onClick={async () => {
-                  await deleteProject(data?._id).then(async () => {
-                    await getAllProjects()
-                      .then((result) => {
-                        setallOpenSourceProjects(result);
-                        console.log("Open Source Projects", result);
-                      })
-                      .catch((error) => {
-                        console.error(error);
-                      });
-                  });
-                }}
-                className=" absolute top-5 cursor-pointer right-5"
+              {user?.emailAddresses[0].emailAddress ==
+                data?.projectOwner?.emailAddresses[0]?.emailAddress && (
+                <div
+                  onClick={async () => {
+                    await deleteProject(data?._id).then(async () => {
+                      await getAllProjects()
+                        .then((result) => {
+                          setallOpenSourceProjects(result);
+                          console.log("Open Source Projects", result);
+                        })
+                        .catch((error) => {
+                          console.error(error);
+                        });
+                    });
+                  }}
+                  className=" absolute top-5 cursor-pointer right-5"
+                >
+                  <XIcon size={17} />
+                </div>
+              )}
+              <p className="text-gray-600 font-semibold ">{data?.projectBy}</p>
+              <p className="mt-2  max-w-xs overflow-x-hidden">
+                {data?.projectDescription}
+              </p>
+              <Button
+                variant={"outline"}
+                className="mt-5 text-sm"
+                onClick={() => handleLearnMore(data)}
               >
-                <XIcon size={17} />
-              </div>
-            )}
-            <p className="text-gray-600 font-semibold">{data?.projectBy}</p>
-            <p className="mt-2">{data?.projectDescription}</p>
-            <Button
-              variant={"outline"}
-              className="mt-5 text-sm"
-              onClick={() => handleLearnMore(data)}
-            >
-              Learn More
-            </Button>
-            {data?.tags && (
-              <div className=" grid grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-                {data?.tags?.map((data: any, i: number) => {
-                  return (
-                    <div
-                      className="bg-gray-200 p-1 rounded-2xl w-fit px-3 text-xs"
-                      key={i}
-                    >
-                      {data}
-                    </div>
-                  );
-                })}
-              </div>
-            )}
-          </div>
-        ))}
+                Learn More
+              </Button>
+              {data?.tags && (
+                <div className=" grid grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+                  {data?.tags?.map((data: any, i: number) => {
+                    return (
+                      <div
+                        className="bg-gray-200 p-1 rounded-2xl w-fit px-3 text-xs"
+                        key={i}
+                      >
+                        {data}
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ProjectsSection;
